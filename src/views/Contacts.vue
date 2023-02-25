ee5a39bff3dca2c89e2cee28e49f57baf6a07a06
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2023-02-26 02:02:37 +0700

    Update Contacts.vue

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -3,7 +3,16 @@
     <div class="container">
       <p class="display-2">Contact Form</p>
 
-        <form method="post" netlify></form>
+      <form method="POST" netlify>
+            <label for="email">Email</label><br />
+            <input type="email" name="email" placeholder="alamat email" />
+            <br />
+            <label for="message">Pesan</label><br />
+            <textarea name="message" placeholder="Tulis pesan anda..." rows="4" cols="80"></textarea>
+            <br />
+            <br />
+            <input type="submit" value="Kirim" />
+        </form>
 
 
     </div>
