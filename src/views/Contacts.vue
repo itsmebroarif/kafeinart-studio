ad77ffdec0c9f43fc0be6033453e0113cd20001e
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2023-02-26 02:10:01 +0700

    Update Contacts.vue

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,53 +1,49 @@
 <template>
   <div class="contacts" style="color:aliceblue;">
     <div class="container">
-      <p class="display-2">Contact Form</p>
-      <form name="contact" netlify>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Name</label>
-          <input
-          for="name"
-          placeholder="Insert Your Name"
-            name="nama"
-            type="name"
-            class="form-control"
-            id="name"
-            aria-describedby="namehelp"
-          />
-          <label for="exampleInputEmail1" class="form-label mt-2"
-            >Email address</label
-          >
-          <input
-          for="email"
-          placeholder="Insert Your Email..."
-            name="email"
-            type="email"
-            class="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
+      <p class="display-2">Contact Me In Below</p>
+
+
+
+
+
+
+
+
+
+      <!-- Baris Sosial media 1 -->
+      <div class="row">
+        <div class="col">
+          <a href="http://" class="btn btn-outline-primary">Social Media</a>
         </div>
-        <div class="mb-3">
-          <label for="messages" class="form-label">Messages</label>
-          <textarea
-          for="messages"
-          placeholder="Enter Message..."
-            type="text"
-            class="form-control"
-            id="messages"
-          />
+        <div class="col">
+          <a href="http://" class="btn btn-outline-primary">Social Media</a>
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1"
-            >i Agree this message will be read into</label
-          >
+        <div class="col">
+          <a href="http://" class="btn btn-outline-primary">Social Media</a>
         </div>
-        <input class="btn btn-outline-primary" type="submit" value="Kirim" />
-      </form>
+      </div>
+      <!-- Baris Sosial media 2 -->
+      <div class="row">
+        <div class="col">
+          <a href="http://" class="btn btn-outline-primary">Social Media</a>
+        </div>
+        <div class="col">
+          <a href="http://" class="btn btn-outline-primary">Social Media</a>
+        </div>
+        <div class="col">
+          <a href="http://" class="btn btn-outline-primary">Social Media</a>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
     </div>
   </div>
 </template>
