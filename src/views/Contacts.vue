afaba8c16ebda8f27dd7b52219ccac74347f52ba
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2023-02-26 02:01:26 +0700

    Update Contacts.vue

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -2,49 +2,10 @@
   <div class="contacts" style="color: aliceblue">
     <div class="container">
       <p class="display-2">Contact Form</p>
-      <form name="contact" method="POST" netlify>
-        <div class="mb-3">
-          <label for="name" class="form-label">Name</label>
-          <input
-            placeholder="Insert Your Name"
-            name="nama"
-            type="name"
-            class="form-control"
-            id="name"
-            aria-describedby="namehelp"
-          />
-          <label for="email" class="form-label mt-2"
-            >Email address</label
-          >
-          <input
-            placeholder="Insert Your Email..."
-            name="email"
-            type="email"
-            class="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="messages" class="form-label">Messages</label>
-          <textarea
-            placeholder="Enter Message..."
-            type="text"
-            class="form-control"
-            id="messages"
-          />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1"
-            >i Agree this message will be read into</label
-          >
-        </div>
-        <input class="btn btn-outline-primary" type="submit" value="Kirim" />
-      </form>
+
+        <form method="post" netlify></form>
+
+
     </div>
   </div>
 </template>
